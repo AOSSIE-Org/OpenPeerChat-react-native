495bd548faf41e1a260f1ed024299242f018dada
Ritik Jain <ritikjain1272@gmail.com>
2021-07-12 03:24:48 +0000

    Feat: Adhoc network implemenation using GossipSub

--- App.js
+++ App.js
@@ -3,25 +3,25 @@ import { NativeModules, Button, View, Text, Alert, DeviceEventEmitter, TextInput
 
 const { NearbyConnection } = NativeModules;
 
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const ID_SIZE = 5;
 
-// Generates a random string twhich serves as an identifer
-// for the endpoint
-const generateString = (length) => {
-    let result = ' ';
+const generateId = (length) => {
+    let id = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        id += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result;
+    return id;
 }
 
 const App = () => {
     const [endpoints, setEndpoints] = useState([]);
     const [messages, setMessages] = useState([]);
     const [msg, setMsg] = useState();
+    const [targetName, setTargetName] = useState("");
 
-    let name = generateString(5);
+    let name = generateId(ID_SIZE);
 
     // Start discovery with given `name`
     const discover = () => {
@@ -41,26 +41,44 @@ const App = () => {
         setEndpoints(event);
     }
 
-    // Sends a message to the given endpoint
-    const sendMessage = (endpointId) => {
-        console.log(endpointId)
-        NearbyConnection.sendMessage(endpointId, msg);
-        setMsg("");
+    const msendMessage = (senderName) => {
+        endpoints.map(endpoint => {
+            let id = endpoint.split('_')[0];
+            let endpointName = endpoint.split('_')[1];
+            if(endpointName !== senderName){
+                let message = msg;
+                message = msg + '%' + targetName + '%' + name;
+                NearbyConnection.sendMessage(id, message);
+            }
+        })
     }
 
-    const receiveMessage = (event) => {
-        alert(JSON.stringify(event));
-        // setMessages([...messages, event]);
+    const mreceiveMessage = (event) => {
+        let data = event['message']
+        // split the message
+        data = data.split('%');
+        let message = data[0];
+        let targetName = data[1];
+        let senderName = data[2];
+        if(name === targetName){
+            alert(`Message: ${message}`);
+            return;
+        }
+        else{
+            alert(`Message is hopped for ${targetName}`);
+            msendMessage(senderName);
+        }
     }
 
     // Listens for endpoints discovered/lost
     DeviceEventEmitter.addListener('endpoints', endpointList);
 
     // Listens for incoming messages
-    DeviceEventEmitter.addListener('message', receiveMessage);
+    DeviceEventEmitter.addListener('message', mreceiveMessage);
 
     return (
         <View style={{ flex: 1 }}>
+            <Text style={{alignSelf: 'center'}}>Name of device: {name}</Text>
             <Button
                 title="Advertise"
                 color='#841584'
@@ -71,10 +89,17 @@ const App = () => {
                 color='#841584'
                 onPress={discover}
             />
-            <TextInput style={{ backgroundColor: 'black', color: 'white' }} value={msg} onChangeText={text => setMsg(text)} />
+            <TextInput style={{ backgroundColor: 'red', color: 'white' }}
+                placeholder="Target Name"
+                placeholderTextColor="white"
+                value={targetName} onChangeText={text => setTargetName(text)} />
+            <TextInput style={{ backgroundColor: 'black', color: 'white' }} value={msg} 
+                placeholder="Message"
+                placeholderTextColor="white"
+                onChangeText={text => setMsg(text)} />
             {endpoints.map((item, key) => {
                 return (
-                    <Button key={key} onPress={() => sendMessage(item)} title={item} />
+                    <Button key={key} onPress={() => msendMessage()} title={item} />
                 );
             })}
         </View>
