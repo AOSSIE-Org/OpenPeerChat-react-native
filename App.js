4ade6259e351badf0c3c6d34eb9db0ab0c371513
harry-dev98 <hs921818@student.nitw.ac.in>
2021-07-28 13:47:52 +0530

    basic integraton

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/Ionicons";
 
+
+import { MatrixServerProvider } from "./src/context/MatrixServer";
 import Splash from "./src/Splash";
 import Name from "./src/Name";
 import Home from "./src/Home";
@@ -13,63 +15,66 @@ import AddChat from "./src/AddChat";
 const Stack = createStackNavigator();
 
 const MainStack = () => {
+  
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Splash"
-        component={Splash}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Name"
-        component={Name}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="AddChat"
-        component={AddChat}
-        options={{
-          title: "Add Contact",
-          headerStyle: {
-            backgroundColor: "#B83227",
-            height: 60,
-          },
-          headerTintColor: "#fff",
-        }}
-      />
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerLeft: () => null,
-          title: "Aossie Chat",
-          headerRight: () => (
-            <View style={{ flexDirection: "row" }}>
-              <Icon
-                name="search"
-                size={25}
-                style={{ marginRight: 20, color: "#fff" }}
-              />
-              <Icon
-                name="md-settings-sharp"
-                size={25}
-                style={{ marginRight: 15, color: "#fff" }}
-              />
-            </View>
-          ),
-          headerStyle: {
-            backgroundColor: "#B83227",
-            height: 80,
-          },
-          headerTintColor: "#fff",
-        }}
-      />
-      <Stack.Screen
-        name="Chat"
-        component={Chat}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
+    <MatrixServerProvider>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Name"
+          component={Name}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AddChat"
+          component={AddChat}
+          options={{
+            title: "Add Contact",
+            headerStyle: {
+              backgroundColor: "#B83227",
+              height: 60,
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerLeft: () => null,
+            title: "Aossie Chat",
+            headerRight: () => (
+              <View style={{ flexDirection: "row" }}>
+                <Icon
+                  name="search"
+                  size={25}
+                  style={{ marginRight: 20, color: "#fff" }}
+                />
+                <Icon
+                  name="md-settings-sharp"
+                  size={25}
+                  style={{ marginRight: 15, color: "#fff" }}
+                />
+              </View>
+            ),
+            headerStyle: {
+              backgroundColor: "#B83227",
+              height: 80,
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </MatrixServerProvider>
   );
 };
 
--- src/Name.js
+++ src/Name.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { MatrixServerContext } from './context/MatrixServer';
 
 const TIME = 1500;
 const LENGTH_OF_ID = 5;
@@ -17,33 +18,82 @@ const generateUId = (length) => {
 };
 
 const Name = ({ navigation }) => {
-  const [name, setName] = useState();
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    password: '',
+    username: '',
+    url: '',
+  });
   const [message, setMessage] = useState();
+  const { server, setServerUrl } = useContext(MatrixServerContext);
+  const handleUrlChange = (e) => {
+    setUserInfo({
+      ...userInfo,
+      url: e
+    });
+  };
   const handleNameChange = (e) => {
-    setName(e);
+    setUserInfo({
+      ...userInfo,
+      name: e
+    });
+  };
+  const handleUsernameChange = (e) => {
+    setUserInfo({
+      ...userInfo,
+      username: e,
+    });
+  };
+  const handlePasswordChange = (e) => {
+    setUserInfo({
+      ...userInfo,
+      password: e,
+    });
   };
   const saveName = async () => {
-    if (name === "") {
-      setMessage("Please enter a valid name");
+    if (userInfo.name === "" || userInfo.username === "" || userInfo.password === "" || userInfo.url === "") {
+      setMessage("Please enter a valid information");
       return;
     }
     try {
       const id = generateUId();
-      const uid = `${name}@${id}`;
-      await AsyncStorage.setItem("username", name);
+      const uid = `${userInfo.name}@${id}`;
+      setServerUrl(userInfo.url);
+      try {
+        await server.login(userInfo.username, userInfo.password)
+      } catch (e) {
+        if(e.errcode === "M_FORBIDDEN") {
+          const res = await server.registration(userInfo.username, userInfo.password);
+          console.log(res);
+        }
+      }
+      await AsyncStorage.setItem("userInfo", JSON.stringify(userInfo));
       await AsyncStorage.setItem("uid", uid);
       navigation.navigate("Home");
     } catch (err) {
       console.error(err);
+      setMessage("Please enter valid details")
     }
   };
   return (
     <View style={styles.container}>
-      <Text>Please enter your username</Text>
+      <Text>Please enter your username and password (SignIn/SignUp)</Text>
       {message !== "" && <Text>{message}</Text>}
+      <TextInput
+        placeholder="Server URL"
+        onChangeText={(e) => handleUrlChange(e)}
+      />
       <TextInput
         placeholder="Your Name"
         onChangeText={(e) => handleNameChange(e)}
+      />
+      <TextInput
+        placeholder="Your Username"
+        onChangeText={(e) => handleUsernameChange(e)}
+      />
+      <TextInput
+        placeholder="Your Password"
+        onChangeText={(e) => handlePasswordChange(e)}
       />
       <Button title="Save" onPress={saveName} />
     </View>
